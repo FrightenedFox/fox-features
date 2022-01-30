d4befba17ba22850ee9b1abc0f14fdca9fe87910
FrightenedFox <vitalij20358@gmail.com>
2022-01-30 21:25:39 +0100

    Add OpenBoard support for HS611

---
+++ Linux/HS611/OpenBoardScheme.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 1 "key +ctrl +y -y -ctrl"
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 2 "key +ctrl +h -h -ctrl"
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 3 "key +ctrl +shift +h -h -shift -ctrl"
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 8 "key +ctrl +j -j -ctrl"
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 9 "key +ctrl +m -m -ctrl"
+
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 10 "key +ctrl +g -g -ctrl"
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 11 "key +ctrl +f -f -ctrl"
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 12 "key +ctrl +down"
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 13 "key +ctrl +up"
+xsetwacom --set 'HUION Huion Tablet_HS611 Pad pad' Button 14 "key +ctrl +z -z -ctrl"
+
+xsetwacom --set 'HUION Huion Tablet_HS611 stylus' Button 1 "button +1"  
+xsetwacom --set 'HUION Huion Tablet_HS611 stylus' Button 2 "key +ctrl +i -i -ctrl"
+xsetwacom --set 'HUION Huion Tablet_HS611 stylus' Button 3 "key +ctrl +e -e -ctrl"
+
+xsetwacom --set 'HUION Huion Tablet_HS611 stylus' PressureCurve 10 0 25 100
---
+++ Linux/HS611/setup_area.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xinput list
+# stylus
+xinput map-to-output 16 DP-4
+# Pad pad
+xinput map-to-output 16 DP-4
